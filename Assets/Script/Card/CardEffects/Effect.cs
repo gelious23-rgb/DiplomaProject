cb361e74edb9eea8067487afa418321ac1149864
Mykhailo Baranets <baranets99934@gmail.com>
2024-02-22 17:59:51 +0100

    Fix merge conflicts

--- Assets/Script/Card/CardEffects/Effect.cs
+++ Assets/Script/Card/CardEffects/Effect.cs
@@ -9,16 +9,12 @@ namespace Script.Card.CardEffects
     public class Effect : MonoBehaviour
     {
         public BattleBehaviour BattleBehaviour;
-<<<<<<< HEAD
         protected void Start()
-=======
-        void Start()
->>>>>>> 6af6e68b54f96baeaf263203283ac3c9dbdd654d
         {
             CardEffectHandler.Effects.Add(this);
             CardEffectHandler.OnTurnStart.AddListener(OnTurnStart);
             CardEffectHandler.OnTurnEnd.AddListener(OnTurnEnd);
-<<<<<<< HEAD
+
            // CardEffectHandler.OnAttack.AddListener(OnAttack);
           //  CardEffectHandler.OnBeingHit.AddListener(OnBeingHit); 
         }
@@ -27,20 +23,12 @@ namespace Script.Card.CardEffects
         {
             BattleBehaviour = FindObjectOfType<BattleBehaviour>();
         }
-
-=======
-            CardEffectHandler.OnAttack.AddListener(OnAttack);
-            CardEffectHandler.OnBeingHit.AddListener(OnBeingHit);
-            
-            BattleBehaviour = FindObjectOfType<BattleBehaviour>();
-        }
         
->>>>>>> 6af6e68b54f96baeaf263203283ac3c9dbdd654d
         public CardInfoDisplay GetCard()
         {
             return GetComponent<CardInfoDisplay>();
         }
-<<<<<<< HEAD
+
 
         public virtual void OnAttack(CardInfoDisplay target, CardInfoDisplay self)
         {
@@ -48,30 +36,20 @@ namespace Script.Card.CardEffects
         }
 
         public virtual void OnBeingHit(CardInfoDisplay target, CardInfoDisplay damageSource)
-=======
-        protected virtual void OnAttack(CardInfoDisplay target)
-        {
-            throw new NotImplementedException();
-        }
-        protected virtual void OnBeingHit(CardInfoDisplay target)
->>>>>>> 6af6e68b54f96baeaf263203283ac3c9dbdd654d
         {
             throw new NotImplementedException();
         }
         protected virtual void OnTurnStart()
         {
-            throw new NotImplementedException();
+            Debug.Log("On turn start worked");
         }
         protected virtual void OnTurnEnd()
         {
             if (DestroyOnTurnEnd == true) 
             {
                 
-<<<<<<< HEAD
                 Destroy(this);
-=======
-                /*Destroy(this);*/
->>>>>>> 6af6e68b54f96baeaf263203283ac3c9dbdd654d
+
             }
             
         }
